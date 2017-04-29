cfad793e0f6a287c984088ee8224bbf380b924a1
Jan Tulak <jan@tulak.me>
2017-04-29 17:28:17 +0200

    better running
    Signed-off-by: Jan Tulak <jan@tulak.me>

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,6 @@ ADD cpachecker.tar.bz2 /cpachecker
 
 workdir /workdir
 volume /workdir
+
+ADD run-test.sh /
+ENTRYPOINT ["/bin/bash","/run-test.sh", "/workdir"]
---
+++ run-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 WORKDIR"
+	exit 1
+fi
+
+cd $1
+
+if [ ! -d "mkfs" ]; then
+	echo "Workdir does not seem to contain xfsprogs"
+	exit 1
+fi
+
+# TODO 
+echo "Nothing implemented..."
